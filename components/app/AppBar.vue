9aff9524e6be9bc3cb007e1e33f0e5ec3da43b6d
angelorc <angelo@rcstart.it>
2023-12-14 16:30:46 +0100

    Remove app logo from AppBar.vue

--- components/app/AppBar.vue
+++ components/app/AppBar.vue
@@ -2,7 +2,6 @@
   <v-app-bar elevation="0" color="#121212">
     <v-btn v-if="mdAndDown" @click.stop="toggleDrawer" variant="text" icon="mdi-menu"></v-btn>
     <div class="d-flex pt-3 pb-1">
-      <div><app-logo> </app-logo></div>
       <div class="ml-3 text-h5 mt-1">Interchain.fm </div>
     </div>
     <template #append>
